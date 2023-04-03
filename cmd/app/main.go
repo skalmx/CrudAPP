e988ded4051060443dda63b2a29baf6c42a7b8e9
skalmx <poplevinwork@gmail.com>
2023-04-03 23:51:00 +0300

    init all files directories and added there empty files

---
+++ cmd/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	
+	
+)
+
+
+func main() {
+	// connection := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s", "localhost", 5432, "postgres", "postgres", "disable", "qwerty12")
+	// db, err := sql.Open("postgres", connection)
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+
+	// if err := db.Ping(); err != nil {
+	// 	log.Print(err)
+	// }
+	
+	// defer db.Close()
+
+	// _, err = db.Exec("insert into Products(model, company, price) values ('IphoneXr','apple', 400)")
+	// if err != nil{
+	// 	fmt.Print(err)
+	// } test of db connection and docker containers
+   
+
+}
